5520bdf069e20ab239dea849fd47ef779854b3c7
Jonathan Sewell <jonathan.sewell@cubeworks.co.uk>
2012-02-15 13:19:00 +0000

    First commit

---
+++ ConsoleApplication1/ConsoleApplication1/Looper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using log4net;
+
+namespace Log4NetExamples.ConsoleApplication1
+{
+	class Looper
+	{
+		private readonly ILog _logger;
+
+		public Looper()
+		{
+			//get the logger for this type
+			_logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		}
+
+		public void Loop()
+		{
+			_logger.Info("Running the loop");
+
+			for (int i = 0; i < 10; i++)
+			{
+				if (i == 7)
+					throw new ApplicationException("I don't like sevens");
+
+				_logger.Info("Loop body " + i);
+			}
+		}
+	}
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+//see http://logging.apache.org/log4net/release/manual/configuration.html#attributes
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace Log4NetExamples.ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//get the logger for this type
+			var logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+			//get the logger by name
+			var emailLogger = log4net.LogManager.GetLogger("EmailLogger");
+
+
+			//simulate some activity and an exception
+			try
+			{
+				var looper = new Looper();
+				looper.Loop();
+			}
+			catch (Exception ex)
+			{
+				emailLogger.Error("Help, something went wrong ... " + ex.Message);
+				logger.Error(ex.Message);
+			}
+		}
+	}
+}
