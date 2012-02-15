119cef47706cb72da3dd3d52dab310c148a07c1a
Jonathan Sewell <jonathan.sewell@cubeworks.co.uk>
2012-02-15 13:31:18 +0000

    Moved Looper to Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,20 +1,21 @@
-﻿
-using System;
+﻿using System;
+using System.Reflection;
+using log4net;
+using log4net.Config;
 
 //see http://logging.apache.org/log4net/release/manual/configuration.html#attributes
-[assembly: log4net.Config.XmlConfigurator(Watch = true)]
-
+[assembly: XmlConfigurator(Watch = true)]
 namespace Log4NetExamples.ConsoleApplication1
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
 			//get the logger for this type
-			var logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+			var logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
 			//get the logger by name
-			var emailLogger = log4net.LogManager.GetLogger("EmailLogger");
+			var emailLogger = LogManager.GetLogger("EmailLogger");
 
 
 			//simulate some activity and an exception
@@ -27,6 +28,30 @@ namespace Log4NetExamples.ConsoleApplication1
 			{
 				emailLogger.Error("Help, something went wrong ... " + ex.Message);
 				logger.Error(ex.Message);
+			}
+		}
+	}
+
+	internal class Looper
+	{
+		private readonly ILog _logger;
+
+		public Looper()
+		{
+			//get the logger for this type
+			_logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+		}
+
+		public void Loop()
+		{
+			_logger.Info("Running the loop");
+
+			for (int i = 0; i < 10; i++)
+			{
+				if (i == 7)
+					throw new ApplicationException("I don't like sevens");
+
+				_logger.Info("Loop body " + i);
 			}
 		}
 	}
